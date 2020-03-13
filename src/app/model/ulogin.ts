0a88893943f30fec793c2759a009010651cf52de
Bindhu <gondipallinagabindu3@gmail.com>
2020-03-13 16:58:23 +0530

    code

---
+++ src/app/model/ulogin.ts
@@ -0,0 +1,6 @@
+export class Ulogin {
+    public userId:number;
+        public userName: string;
+        public userpassword: string;
+
+}
---
+++ src/app/service/product.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../entities/product.entity';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  public products: Product[];
+  constructor() {
+    this.products = [
+      { id: 'p1', name: 'CASUAL SHIRT', price: 1600, photo: 'assets/images/m1.jpg '},
+      { id: 'p2', name: 'KURTHA SET', price: 1200, photo: 'assets/images/m2.jpg' },
+      { id: 'p3', name: 'JEANS', price: 1300, photo: 'assets/images/m4.jpg' },
+      { id: 'p4', name: 'WINTER JACKET', price: 3200, photo: 'assets/images/men3.png' },
+      {id: 'p5',name: 'IPHONE',price:5000,photo:'assets/images/eg1.jpg'}
+
+  ];
+}
+
+
+findAll(): Product[] {
+  return this.products;
+}
+
+find(id: string): Product {
+  return this.products[this.getSelectedIndex(id)];
+}
+
+public getSelectedIndex(id: string) {
+  for (var i = 0; i < this.products.length; i++) {
+      if (this.products[i].id == id) {
+          return i;
+      }
+  }
+  return -1;
+}
+
+}
+
+
+   
+  
+
+  
+
